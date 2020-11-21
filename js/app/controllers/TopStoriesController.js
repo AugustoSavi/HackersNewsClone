b30050b14f7bbe15fb75d54b2901f784fa82b636
Augusto Savi <guto_savi@outlook.com>
2020-11-21 19:19:25 -0300

    adcionado numero de comentarios

---
+++ js/app/controllers/TopStoriesController.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+    
+      angular
+        .module('app')
+        .controller('TopStoriesController', TopStoriesController);
+    
+      TopStoriesController.$inject = ['TopStoriesService'];
+      function TopStoriesController(TopStoriesService) {
+        var vm = this;
+        vm.stories = [];
+        
+    
+        activate();
+    
+        ////////////////
+    
+        function activate() {
+          TopStoriesService 
+            .getStories()
+            .then(function(res) {
+              vm.stories = res.data;
+            });
+    
+        }
+      }
+    
+    })();
---
+++ js/app/directives/Story.js
@@ -0,0 +1,84 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('story', story);
+
+    story.$inject = ['TopStoriesService'];
+    function story(TopStoriesService) {
+        // Usage:
+        //
+        // Creates:
+        //
+        var directive = {
+            bindToController: true,
+            controller: StoryDirectiveController,
+            controllerAs: 'vm',
+            link: link,
+            templateUrl:'views/story.html',
+            restrict: 'E',
+            scope: {
+                id: '=id'
+            }
+        };
+        return directive;
+        
+        function link(scope, element, attrs) {
+            var hideLink = element[0].querySelector('.hide');
+
+            hideLink.addEventListener('click', function(event){
+              element.parent().remove();
+            });
+            
+            scope.$on('$destroy', function(){
+                elem.off();
+            });
+        }
+    }
+    /* @ngInject */
+    function StoryDirectiveController (TopStoriesService) {
+        const vm = this;
+
+        function getDomainFromUrl(url){
+            var a = document.createElement("a");
+            a.setAttribute('href',url);
+
+            if (a.hostname === 'news.ycombinator.com'){
+                return '';
+            }
+            else {
+                return '('+ a.hostname + ')';
+            }
+        }
+
+        function getHoursAgo(seconds) {
+            var secondDifference = (Date.now()/1000) - seconds;
+            
+            if (secondDifference < 3600) {
+              var minutesAgo = secondDifference/60;
+              return Math.floor(minutesAgo) + ' minutes ago';
+            } else {
+              var hoursAgo = secondDifference/3600;
+              return Math.floor(hoursAgo) + ' hours ago';
+            }
+        }
+
+        TopStoriesService
+        .getStory(vm.id)
+        .then(function(res){
+            vm.story = res.data;
+            vm.title = vm.story.title;
+            vm.url = vm.story.url;
+            if(!vm.url){
+                vm.url = 'https://news.ycombinator.com/item?id='+ vm.story.id;
+            }
+            vm.domain = getDomainFromUrl(vm.url);
+            vm.score = vm.story.score;
+            vm.author = vm.story.by;
+            vm.time = getHoursAgo(vm.story.time);
+            vm.id = vm.story.id;
+            vm.numComments = vm.story.descendants;
+        })
+    }
+})();
